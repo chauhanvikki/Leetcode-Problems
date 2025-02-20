c35f5697ed782fa12d56eb120219b7e023c8211b
Vishvendra Singh <112617024+chauhanvikki@users.noreply.github.com>
2025-02-20 21:53:23 +0530

    Time: 2 ms (88.01%), Space: 55.5 MB (62.29%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -8,7 +8,6 @@ class Solution {
         for(int i=1;i<n;i++){
             pre[i]=pre[i-1]*nums[i];
         }
-        // ans[0]=suff[0];
         for(int i=n-2;i>=0;i--){
             suff[i]=nums[i]*suff[i+1];
         }
