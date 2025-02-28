4eb0901d344d01025366ca06d4f89decbd77a352
Vishvendra Singh <112617024+chauhanvikki@users.noreply.github.com>
2025-02-28 21:38:26 +0530

    Time: 0 ms (100%), Space: 40.8 MB (94.08%) - LeetHub

--- 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.java
+++ 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.java
@@ -13,26 +13,12 @@ class Solution {
         if(head==null){
             return null;
         }
-        // int length=count(head);
         ListNode temp1=head;
         ListNode temp2=head;
-        // ListNode temp3=temp1.next;
         while(temp2!=null && temp2.next!=null){
             temp1=temp1.next;
             temp2=temp2.next.next;
-            // temp3=temp3.next;
         }
-        // if(length%2==0){
-        //      return temp3;
-        // }
         return temp1;
     }
-    // public int count(ListNode c){
-    //     int l=0;
-    //     while(c!=null){
-    //         c=c.next;
-    //         l++;
-    //     }
-    //     return l;
-    // }
 }
