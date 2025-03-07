95f3913a770866aa92b111c4ed8940c4a81be904
Vishvendra Singh <112617024+chauhanvikki@users.noreply.github.com>
2025-03-07 21:45:41 +0530

    Time: 4 ms (96.56%), Space: 46.3 MB (27.92%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.java
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.java
@@ -6,11 +6,13 @@ class Solution {
             int m=l+(r-l)/2;
             if(x-arr[m]<=arr[m+k]-x){
                 r=m;
-            }else{
-            l=m+1;}
+            }
+            else{
+                l=m+1;
+            }
         }
         List<Integer> res=new ArrayList<>();
-        for(int i=l;i<l+k;i++){
+        for(int i=l;i<k+l;i++){
             res.add(arr[i]);
         }
         return res;
