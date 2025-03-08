b66dd610576380e6ad5810850b0d6071ef62619c
Vishvendra Singh <112617024+chauhanvikki@users.noreply.github.com>
2025-03-08 20:13:50 +0530

    Time: 1 ms (98.41%), Space: 44.8 MB (60.62%) - LeetHub

--- 1168-duplicate-zeros/1168-duplicate-zeros.java
+++ 1168-duplicate-zeros/1168-duplicate-zeros.java
@@ -3,20 +3,6 @@ class Solution {
         int n=arr.length;
         int[] res=new int[n];
         int idx=0;
-        // int j=0;
-        // while(i<n && j<n){
-        //     if(arr[i]==0 && j+1<n){
-        //         res[j]=arr[i];
-        //         res[j+1]=arr[i];
-        //         j=j+2;
-        //         i++;
-        //     }
-        //     else{
-        //         res[j]=arr[i];
-        //         j++;
-        //         i++;
-        //     }
-        // }
         for(int i=0;i<n && idx<n;i++){
             if(arr[i]==0){
                 // res[idx]=arr[i];
@@ -30,6 +16,5 @@ class Solution {
         for(int k=0;k<n;k++){
                 arr[k]=res[k];
             }
-        System.out.println(Arrays.toString(arr));
     }
 }
