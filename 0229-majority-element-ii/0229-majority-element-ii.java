1ac97cd73c0e79c6c22c54d35b728c72059d5341
Vishvendra Singh <112617024+chauhanvikki@users.noreply.github.com>
2025-03-14 20:05:56 +0530

    Time: 2 ms (99.18%), Space: 48.3 MB (40.84%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,40 +1,58 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        // int cand=nums[0];
-        // int count=0;
-        // for(int i=0;i<nums.length;i++){
-        //     if(count==0){
-        //         cand=nums[i];
-        //     }
-        //     if(nums[i]==cand){
-        //         count++;
-        //     }
-        //     else{
-        //         count--;
-        //     }
-        // }
-        // count=0;
-        // List<Integer> l=new ArrayList<>();
-        // for(int i:nums){
-        //     if(i==cand){
-        //         count++;
-        //     }
-        // }
-        // if(count<nums.length/3){
-        //     l.add(cand);
-        // }
-        // return l;
-        List<Integer> l=new ArrayList<>();
-        HashMap<Integer,Integer> hm=new HashMap<>();
-        for(int i:nums){
-            hm.put(i,hm.getOrDefault(i,0)+1);
-        }
-        int check=nums.length/3;
-        for(int i: hm.keySet()){
-            if(hm.get(i)>check){
-                l.add(i);
+        int cand1=Integer.MIN_VALUE;
+        int cand2=Integer.MIN_VALUE;
+        int count1=0;
+        int count2=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==cand1){
+                count1++;
+            }
+            else if(nums[i]==cand2){
+                count2++;
+            }
+            else if(count1==0){
+                cand1=nums[i];
+                count1=1;
+            }
+            else if(count2==0){
+                cand2=nums[i];
+                count2=1;
+            }
+            else{
+                count1--;
+                count2--;
             }
         }
+        count1=0;
+        count2=0;
+        List<Integer> l=new ArrayList<>();
+        for(int i:nums){
+            if(i==cand1){
+                count1++;
+            }
+            if(i==cand2){
+                count2++;
+            }
+        }
+        if(count1>nums.length/3 ){
+            l.add(cand1);
+        }
+        if(count2>nums.length/3 && cand1!=cand2){
+            l.add(cand2);
+        }
         return l;
+        // List<Integer> l=new ArrayList<>();
+        // HashMap<Integer,Integer> hm=new HashMap<>();
+        // for(int i:nums){
+        //     hm.put(i,hm.getOrDefault(i,0)+1);
+        // }
+        // int check=nums.length/3;
+        // for(int i: hm.keySet()){
+        //     if(hm.get(i)>check){
+        //         l.add(i);
+        //     }
+        // }
+        // return l;
     }
 }
