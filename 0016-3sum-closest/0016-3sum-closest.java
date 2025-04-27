7fff6b4de5dd377dfc6a850441daabe9acf59311
Vishvendra Singh <112617024+chauhanvikki@users.noreply.github.com>
2025-04-27 14:05:44 +0530

    Time: 12 ms (96%), Space: 43 MB (95.37%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.java
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -1,29 +1,59 @@
+// class Solution {
+//     public int threeSumClosest(int[] nums, int target) {
+//         // HashSet<List<Integer>> ls=new HashSet<>();
+//         int n=nums.length;
+//         int ans=0;
+//         int min=Integer.MAX_VALUE;
+//         Arrays.sort(nums);
+//         for(int i=0;i<n-2;i++){
+//             int j=i+1;
+//             int k=n-1;
+//             while(j<k){
+//                 int sum=nums[i]+nums[j]+nums[k];
+//                 if(Math.abs(target-sum)<min){
+//                     ans=sum;
+//                     min=Math.abs(sum-target);
+//                 }
+//                 if(target>sum){
+//                     j++;
+//                 }
+//                 if(target<sum){
+//                     k--;
+//                 }
+//                 else{
+//                     return target;
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// }
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
-        int n=nums.length;
-        int ans=0;
-        int min=Integer.MAX_VALUE;
         Arrays.sort(nums);
+        int n=nums.length;
+        int minDiff=Integer.MAX_VALUE;
+        int ans=0;
         for(int i=0;i<n-2;i++){
-            int j=i+1;
-            int k=n-1;
-            while(j<k){
-                int sum=nums[i]+nums[j]+nums[k];
-                if(min>Math.abs(sum-target)){
-                    min=Math.min(min,Math.abs(sum-target));
-                    ans=sum;
+            int low=i+1,high=n-1;
+            while(low<high){
+                int temp=nums[i]+nums[low]+nums[high];
+                if(Math.abs(target-temp)<minDiff){
+                    ans=temp;
+                    minDiff=Math.abs(target-temp);
                 }
-                if(target>sum){
-                    j++;
+                if(temp==target){
+                    return target;
                 }
-                if(target<sum){
-                    k--;
+                else if(temp>target){
+                        high--;
                 }
                 else{
-                    return sum;
+                    low++;
                 }
             }
         }
         return ans;
-    }
+            
+    }       
 }
