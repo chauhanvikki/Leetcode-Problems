6110b5806e104d161ea6671ae46c8ca92650fec4
Vishvendra Singh <112617024+chauhanvikki@users.noreply.github.com>
2025-05-05 19:46:56 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -0,0 +1,48 @@
+class Solution {
+    static class pair{
+        int x;
+        int y;
+        pair(int x,int y){
+            this.x=x;
+            this.y=y;
+        }
+    }
+    public int orangesRotting(int[][] grid) {
+        Queue<pair> q=new LinkedList<>();
+        int rotten=0,total=0,time=0;
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j]==1 || grid[i][j]==2)total++;
+                if(grid[i][j]==2)q.offer(new pair(i,j));
+            }
+        }
+            if(total==0)return 0;
+            while(!q.isEmpty()){
+                int size=q.size();
+                rotten+=size;
+                if(rotten==total)return time;
+                time++;
+                for(int i=0;i<size;i++){
+                    pair p=q.peek();
+                    if(p.x+1 < grid.length && grid[p.x+1][p.y]==1){
+                        grid[p.x+1][p.y]=2;
+                        q.offer(new pair(p.x+1,p.y));      
+                    }
+                    if(p.x-1 >= 0 && grid[p.x-1][p.y]==1){
+                        grid[p.x-1][p.y]=2;
+                        q.offer(new pair(p.x-1,p.y));      
+                    }
+                    if(p.y+1 < grid.length && grid[p.x][p.y+1]==1){
+                        grid[p.x][p.y+1]=2;
+                        q.offer(new pair(p.x,p.y+1));      
+                    }
+                    if(p.y-1 >= 0 && grid[p.x][p.y-1]==1){
+                        grid[p.x][p.y-1]=2;
+                        q.offer(new pair(p.x,p.y-1));      
+                    }
+                    q.poll();
+                }
+            }
+        return -1;
+    }
+}
