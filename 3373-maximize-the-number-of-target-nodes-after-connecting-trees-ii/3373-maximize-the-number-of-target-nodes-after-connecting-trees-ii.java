09ac579688a66fd42118c751f802d2473fd5f15c
Vishvendra Singh <112617024+chauhanvikki@users.noreply.github.com>
2025-05-29 15:41:45 +0530

    Time: 105 ms (77.97%), Space: 120.3 MB (71.19%) - LeetHub

---
+++ 3373-maximize-the-number-of-target-nodes-after-connecting-trees-ii/3373-maximize-the-number-of-target-nodes-after-connecting-trees-ii.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int[] maxTargetNodes(int[][] edges1, int[][] edges2) {
+        int n=edges1.length+1;
+        int m=edges2.length+1;
+        int color1[]=new int[n];
+        int color2[]=new int[m];
+        int count1[]=build(edges1,color1);
+        int count2[]=build(edges2,color2);
+        int res[]=new int[n];
+        for(int i=0;i<n;i++){
+            res[i]=count1[color1[i]]+Math.max(count2[0],count2[1]);
+        }
+        return res;
+    }
+    private int[] build(int[][] edges, int color[]){
+        int n=edges.length+1;
+        List<List<Integer>> ls=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            ls.add(new ArrayList<>());
+        }
+        for(int edge[] : edges){
+            ls.get(edge[0]).add(edge[1]);
+            ls.get(edge[1]).add(edge[0]);
+        }
+        int res=dfs(0,-1,0,ls,color);
+        return new int[]{res,n-res};
+    }
+    private int dfs(int node,int parent, int depth, List<List<Integer>> ls,int color[]){
+        int res=1-(depth%2);
+        color[node]=depth%2;
+        for(int child: ls.get(node)){
+            if(child==parent){
+                continue;
+            }
+            res+=dfs(child,node,depth+1,ls,color);
+        }
+        return res;
+    }
+}
