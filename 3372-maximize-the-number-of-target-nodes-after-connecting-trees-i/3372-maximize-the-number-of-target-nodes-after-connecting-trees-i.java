871a06419ebb8337825749331f28bf952c283cbc
Vishvendra Singh <112617024+chauhanvikki@users.noreply.github.com>
2025-06-02 16:41:33 +0530

    Time: 281 ms (81.61%), Space: 46 MB (80.12%) - LeetHub

--- 3372-maximize-the-number-of-target-nodes-after-connecting-trees-i/3372-maximize-the-number-of-target-nodes-after-connecting-trees-i.java
+++ 3372-maximize-the-number-of-target-nodes-after-connecting-trees-i/3372-maximize-the-number-of-target-nodes-after-connecting-trees-i.java
@@ -2,76 +2,47 @@ class Solution {
     public int[] maxTargetNodes(int[][] edges1, int[][] edges2, int k) {
         int n=edges1.length+1;
         int m=edges2.length+1;
+        int[] res=new int[n];
         int[] count1=build(edges1,k);
         int[] count2=build(edges2,k-1);
-        int maxCount2=0;
-        for(int c:count2){
-            if(maxCount2<c){
-                maxCount2=c;
+        int maxi=0;
+        for(int i=0;i<m;i++){
+            if(maxi<count2[i]){
+                maxi=count2[i];
             }
         }
-        int[] res=new int[n];
         for(int i=0;i<n;i++){
-            res[i]=count1[i]+maxCount2;
+            res[i]=count1[i]+maxi;
         }
         return res;
     }
-
-    private int[] build(int[][] edges, int k){
-        int n=edges.length+1;
+    private int[] build(int[][] edges,int k){
         List<List<Integer>> ls=new ArrayList<>();
+        int n=edges.length+1;
+        int[] res=new int[n];
         for(int i=0;i<n;i++){
             ls.add(new ArrayList<>());
         }
-        for(int[] edge : edges){
+        for(int[] edge:edges){
             ls.get(edge[0]).add(edge[1]);
             ls.get(edge[1]).add(edge[0]);
         }
-        int[] res=new int[n];
         for(int i=0;i<n;i++){
-            res[i]=bfs(i,ls,k);
+            res[i]=dfs(i,-1,k,ls);
         }
         return res;
     }
-    private int bfs(int start, List<List<Integer>> ls, int k){
-        int count=0;
-        Queue<int[]> q=new LinkedList<>();
-        boolean vis[]=new boolean[ls.size()];
-
-        q.offer(new int[]{start,0});
-        vis[start]=true;
-
-        while(!q.isEmpty()){
-            int[] curr=q.poll();
-            int node=curr[0];
-            int dist=curr[1];
-            if(dist>k){
+    private int dfs(int node,int parent,int k,List<List<Integer>> ls){
+        if(k<0){
+            return 0;
+        }
+        int res=1;
+        for(int child : ls.get(node)){
+            if(child==parent){
                 continue;
             }
-            count++;
-
-            for(int neighbor: ls.get(node)){
-                if(!vis[neighbor]){
-                    vis[neighbor]=true;
-                    q.offer(new int[]{neighbor,dist+1});
-                }
-            }
-            
+            res+=dfs(child,node,k-1,ls);
         }
-        return count;
+        return res;
     }
-    
-//     private int dfs(int node, int parent, List<List<Integer>> ls, int k){
-//         if(k<0){
-//             return 0;
-//         }
-//         int res=1;
-//         for(int child: ls.get(node)){
-//             if(child==parent){
-//                 continue;
-//             }
-//             res+=dfs(child,node,ls,k-1);
-//         }
-//         return res;
-//     }
 }
