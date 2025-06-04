fdb477a6d31c3e7c2b97c5bbbcd1e5a8b3bca87c
Vishvendra Singh <112617024+chauhanvikki@users.noreply.github.com>
2025-06-04 18:16:27 +0530

    Time: 1 ms (100%), Space: 57.2 MB (50.74%) - LeetHub

--- 1298-maximum-candies-you-can-get-from-boxes/1298-maximum-candies-you-can-get-from-boxes.java
+++ 1298-maximum-candies-you-can-get-from-boxes/1298-maximum-candies-you-can-get-from-boxes.java
@@ -1,32 +1,32 @@
 class Solution {
-    public int maxCandies(int[] status,int[] candies,int[][] keys,int[][] containedBoxes,int[] initialBoxes) {
-
-        int count=0; // Total candies collected
-        boolean[] vis=new boolean[status.length]; // Track visited boxes
+    public int maxCandies(int[] status, int[] candies, int[][] keys, int[][] containedBoxes, int[] initialBoxes) {
+        int total=0;
+        int n=status.length;
+        boolean vis[]=new boolean[n];
         for(int v:initialBoxes){
             dfs(v,status,keys,containedBoxes,vis);
         }
-
-        for(int i=0;i<candies.length;i++){
-            if(vis[i]&&status[i]==1){
-                count+=candies[i];
+        for(int i=0;i<n;i++){
+            if(vis[i] && status[i]==1){
+                total+=candies[i];
             }
         }
-        return count;
+        return total;
     }
-
-    public void dfs(int v,int[] status,int[][] keys,int[][] containedBoxes,boolean[] vis){ 
-
-        vis[v]=true; // Mark the current box as visited
-        for(int vKey:keys[v]){
-            if(vKey==v) continue; // Skip self-key
-            status[vKey]=1; // Unlock the box
+    private void dfs(int v,int[] status,int[][] keys,int [][] containedBoxes, boolean vis[]){
+        vis[v]=true;
+        for(int vKey : keys[v]){
+            if(vKey==v){
+                continue;
+            }
+            status[vKey]=1;
         }
 
-        for(int vContained:containedBoxes[v]){
-            if(!vis[vContained]){
-                dfs(vContained,status,keys,containedBoxes,vis);
+        for(int contain : containedBoxes[v]){
+            if(!vis[contain]){
+                dfs(contain,status,keys,containedBoxes,vis);
             }
         }
+        return;
     }
 }
