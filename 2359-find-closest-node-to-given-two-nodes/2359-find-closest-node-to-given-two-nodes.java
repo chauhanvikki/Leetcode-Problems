d56030dd65f7031025687cf6b63e9b97948d631b
Vishvendra Singh <112617024+chauhanvikki@users.noreply.github.com>
2025-06-08 17:19:05 +0530

    Time: 18 ms (39.55%), Space: 64.7 MB (19.84%) - LeetHub

--- 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.java
+++ 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.java
@@ -6,17 +6,16 @@ class Solution {
             dist1[i]=Integer.MAX_VALUE;
             dist2[i]=Integer.MAX_VALUE;
         }
-        boolean[] vis1=new boolean[edges.length];
-        boolean[] vis2=new boolean[edges.length];
-
+        boolean vis1[]=new boolean[edges.length];
+        boolean vis2[]=new boolean[edges.length];
         dfs(edges,node1,0,dist1,vis1);
         dfs(edges,node2,0,dist2,vis2);
 
         int minVal=Integer.MAX_VALUE;
         int result=-1;
         for(int i=0;i<edges.length;i++){
+            int maxVal=Math.max(dist1[i],dist2[i]);
             if(dist1[i]!=Integer.MAX_VALUE && dist2[i]!=Integer.MAX_VALUE){
-                int maxVal=Math.max(dist1[i],dist2[i]);
                 if(maxVal<minVal){
                     minVal=maxVal;
                     result=i;
@@ -26,9 +25,9 @@ class Solution {
                 }
             }
         }
-        return result;     
+        return result;
     }
-    private void dfs(int[] edges,int node,int depth,int[] dist,boolean vis[]){
+    private void dfs(int[] edges, int node, int depth,int[] dist,boolean[] vis){
         if(node==-1 || vis[node]){
             return;
         }
