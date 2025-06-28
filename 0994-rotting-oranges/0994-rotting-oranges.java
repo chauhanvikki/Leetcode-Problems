e1633017ec88736de5a0cdc220665cbf85e63cba
Vishvendra Singh <112617024+chauhanvikki@users.noreply.github.com>
2025-06-28 19:09:19 +0530

    Time: 1 ms (100%), Space: 42.5 MB (62.5%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,5 +1,5 @@
 class Solution {
-    static class pair{
+    class pair{
         int x;
         int y;
         pair(int x,int y){
@@ -8,35 +8,26 @@ class Solution {
         }
     }
     public int orangesRotting(int[][] grid) {
+        int rotten=0;
         Queue<pair> q=new LinkedList<>();
+        int total=0;
+        int times=0;
         int m=grid.length;
         int n=grid[0].length;
-        int total=0;
-        int time=0;
-        int rotten=0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(grid[i][j] == 2){
+                if(grid[i][j]==2){
                     q.offer(new pair(i,j));
                     total++;
                 }
-                if(grid[i][j]==1){
-                    total++;
-                }
+                else if(grid[i][j]==1)total++;
             }
         }
-        if(total==0){
-            return 0;
-        }
-            while(!q.isEmpty()){
-                int size=q.size();
-                rotten+=size;
-                
-                if(rotten==total){
-                   return time;
-                }
-                time++;
-                for(int i=0;i<size;i++){
+        while(!q.isEmpty()){
+            int size=q.size();
+            rotten+=size;
+            if(rotten==total)return times;
+            for(int i=0;i<size;i++){
                 pair p=q.peek();
                 if(p.x-1>=0 && grid[p.x-1][p.y]==1){
                     grid[p.x-1][p.y]=2;
@@ -54,9 +45,13 @@ class Solution {
                     grid[p.x][p.y+1]=2;
                     q.offer(new pair(p.x,p.y+1));
                 }
-                q.poll();
-                }
+                q.remove();
             }
-            return -1;
+            times++;
+        }
+        if(rotten==0 && total==0){
+            return 0;
+        }
+        return -1;
     }
 }
