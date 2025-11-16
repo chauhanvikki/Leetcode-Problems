92afe336832577673636004eeb0e25e1b81e3a35
Vishvendra Singh <112617024+chauhanvikki@users.noreply.github.com>
2025-11-17 00:41:25 +0530

    Time: 4 ms (90.05%), Space: 46.2 MB (17.8%) - LeetHub

---
+++ 1636-number-of-substrings-with-only-1s/1636-number-of-substrings-with-only-1s.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int numSub(String s) {
+        long ans=0;
+        long count=0;
+        int mod=1000000007;
+        for(char ch:s.toCharArray()){
+            if(ch=='1'){
+                count+=1;
+                ans=(ans+count)%mod;
+            }
+            else{
+                count=0;
+            }
+        }
+        return (int)ans;
+    }
+}
