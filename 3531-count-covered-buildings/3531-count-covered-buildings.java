f36b4b0b9c05930e88bdee8dabdfddf692ebd58a
Vishvendra Singh <112617024+chauhanvikki@users.noreply.github.com>
2025-12-11 23:25:35 +0530

    Time: 609 ms (9.47%), Space: 230.4 MB (53.69%) - LeetHub

--- 3531-count-covered-buildings/3531-count-covered-buildings.java
+++ 3531-count-covered-buildings/3531-count-covered-buildings.java
@@ -2,21 +2,21 @@ class Solution {
     public int countCoveredBuildings(int n, int[][] buildings) {
         HashMap<Integer,TreeSet<Integer>> rowToCol=new HashMap<>();
         HashMap<Integer,TreeSet<Integer>> colToRow=new HashMap<>();
-        for(int[] building:buildings){
+        for(int[] building: buildings){
             int x=building[0];
             int y=building[1];
             rowToCol.computeIfAbsent(x,k -> new TreeSet<>()).add(y);
             colToRow.computeIfAbsent(y,k -> new TreeSet<>()).add(x);
         }
         int count=0;
-        for(int[] building:buildings){
+        for(int[] building: buildings){
             int x=building[0];
             int y=building[1];
             Integer left=rowToCol.get(x).lower(y);
             Integer right=rowToCol.get(x).higher(y);
             Integer up=colToRow.get(y).lower(x);
             Integer down=colToRow.get(y).higher(x);
-            if(left!=null && right!=null && up!=null && down!=null){
+            if(left!=null && right!=null && down!=null && up!=null){
                 count++;
             }
         }
