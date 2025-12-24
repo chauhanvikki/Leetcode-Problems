d06f4d2c3c6be1d564bc2ff13bb41972ab45fc6b
Vishvendra Singh <112617024+chauhanvikki@users.noreply.github.com>
2025-12-24 16:02:44 +0530

    Time: 4 ms (82.79%), Space: 44.8 MB (5.34%) - LeetHub

---
+++ 3074-apple-redistribution-into-boxes/3074-apple-redistribution-into-boxes.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minimumBoxes(int[] apple, int[] capacity) {
+        int sum=0;
+        for(int i=0;i<apple.length;i++){
+            sum+=apple[i];
+        }
+        Arrays.sort(capacity);
+        int count=0;
+        for(int j=capacity.length-1;j>=0;j--){
+            if(sum<=0)return count;
+            sum-=capacity[j];
+            count++;
+        }
+        return count;
+    }
+}
