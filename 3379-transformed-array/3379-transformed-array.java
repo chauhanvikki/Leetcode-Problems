ac95692fe8cf80d27f20cde643339df38e690e8d
Vishvendra Singh <112617024+chauhanvikki@users.noreply.github.com>
2026-02-05 19:25:58 +0530

    Time: 1 ms (98.72%), Space: 46.9 MB (30.77%) - LeetHub

--- 3379-transformed-array/3379-transformed-array.java
+++ 3379-transformed-array/3379-transformed-array.java
@@ -2,7 +2,6 @@ class Solution {
     public int[] constructTransformedArray(int[] nums) {
         int n=nums.length;
         int[] result=new int[n];
-        int index=0;
         for(int i=0;i<n;i++){
             int a=nums[i]%n;
             int newIndex=a+i;
