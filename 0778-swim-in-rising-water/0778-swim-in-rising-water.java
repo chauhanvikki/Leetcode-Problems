3788a05d121bb6a38d7d0ae02372ba11e30d706f
Vishvendra Singh <112617024+chauhanvikki@users.noreply.github.com>
2026-03-06 16:49:54 +0530

    Time: 14 ms (32.3%), Space: 46.3 MB (75.71%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.java
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.java
@@ -10,13 +10,12 @@ class Solution {
         }
     }
     public int swimInWater(int[][] grid) {
+        PriorityQueue<Pair> pq=new PriorityQueue<>((a,b)->a.cost-b.cost);
+        int dr[]={1,-1,0,0};
+        int dc[]={0,0,1,-1};
         int n=grid.length;
         boolean[][] vis=new boolean[n][n];
-        int ans=0;
-        PriorityQueue<Pair> pq=new PriorityQueue<>((a,b)->a.cost-b.cost);
-        pq.offer(new Pair(0,0,grid[0][0]));
-        int[] dr={1,-1,0,0};
-        int[] dc={0,0,1,-1};
+        pq.add(new Pair(0,0,grid[0][0]));
         while(!pq.isEmpty()){
             Pair curr=pq.poll();
             int row=curr.i;
@@ -28,9 +27,9 @@ class Solution {
             for(int i=0;i<4;i++){
                 int nr=row+dr[i];
                 int nc=col+dc[i];
-                if(nr>=0 && nr<n && nc>=0 && nc<n){
-                    int newCost=Math.max(grid[nr][nc],cost);
-                    pq.offer(new Pair(nr,nc,newCost));
+                if(nr>=0 && nc>=0 && nr<n && nc<n){
+                    int maxCost=Math.max(cost,grid[nr][nc]);
+                    pq.offer(new Pair(nr,nc,maxCost));
                 }
             }
         }
