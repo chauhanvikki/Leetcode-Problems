be4fbea430126e4f21e60e96c71a1032330bdd63
Vishvendra Singh <112617024+chauhanvikki@users.noreply.github.com>
2026-03-27 16:58:42 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
@@ -8,7 +8,6 @@ class Solution {
                 maxOpen++;
             }
             else if(ch==')'){
-                // if(maxOpen<0)return false;
                 minOpen--;
                 if(minOpen<0)minOpen=0;
                 maxOpen--;
