cdd0c9541f4a32e31a21c460014e563d71c1acc4
Vishvendra Singh <112617024+chauhanvikki@users.noreply.github.com>
2026-03-27 16:54:44 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
@@ -1,35 +1,57 @@
 class Solution {
     public boolean checkValidString(String s) {
-        Stack<Integer> left=new Stack<>();
-        Stack<Integer> star=new Stack<>();
+        int minOpen=0,maxOpen=0;
         for(int i=0;i<s.length();i++){
             char ch=s.charAt(i);
             if(ch=='('){
-                left.add(i);
+                minOpen++;
+                maxOpen++;
             }
             else if(ch==')'){
-                if(!left.isEmpty()){
-                    left.pop();
-                }else if(!star.isEmpty()){
-                    star.pop();
-                }else{
-                    return false;
-                }
+                minOpen--;
+                if(minOpen<0)minOpen=0;
+                maxOpen--;
+                if(maxOpen>0)return false;
             }
             else if(ch=='*'){
-                star.add(i);
+                minOpen--;
+                minOpen=Math.max(minOpen,0);
+                maxOpen--;
             }
         }
-        while(!left.isEmpty() && !star.isEmpty()){
-            if(left.peek()<star.peek()){
-                left.pop();
-                star.pop();
-            }
-            else{
-                star.pop();
-            }
-        }
-        if(left.isEmpty())return true;
+        if(minOpen==0)return true;
         return false;
+
+        // Stack<Integer> left=new Stack<>();
+        // Stack<Integer> star=new Stack<>();
+        // for(int i=0;i<s.length();i++){
+        //     char ch=s.charAt(i);
+        //     if(ch=='('){
+        //         left.add(i);
+        //     }
+        //     else if(ch==')'){
+        //         if(!left.isEmpty()){
+        //             left.pop();
+        //         }else if(!star.isEmpty()){
+        //             star.pop();
+        //         }else{
+        //             return false;
+        //         }
+        //     }
+        //     else if(ch=='*'){
+        //         star.add(i);
+        //     }
+        // }
+        // while(!left.isEmpty() && !star.isEmpty()){
+        //     if(left.peek()<star.peek()){
+        //         left.pop();
+        //         star.pop();
+        //     }
+        //     else{
+        //         star.pop();
+        //     }
+        // }
+        // if(left.isEmpty())return true;
+        // return false;
     }
 }
