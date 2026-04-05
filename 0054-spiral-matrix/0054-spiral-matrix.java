c26d11995a6254b178b2fbc692b4066c39e97d90
Vishvendra Singh <112617024+chauhanvikki@users.noreply.github.com>
2026-04-05 11:39:18 +0530

    Time: 0 ms (100%), Space: 43.1 MB (51.36%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -5,22 +5,22 @@ class Solution {
         int row=0,col=0;
         List<Integer> ls=new ArrayList<>();
         while(row<=rowLen && col<=colLen){
-        for(int i=col;i<=colLen;i++){
-            ls.add(matrix[row][i]);
-        }
-        for(int i=row+1;i<=rowLen;i++){
-            ls.add(matrix[i][colLen]);
-        }
-        if(row<rowLen){
-            for(int i=colLen-1;i>=col;i--){
-                ls.add(matrix[rowLen][i]);
+            for(int i=col;i<=colLen;i++){
+                ls.add(matrix[row][i]);
             }
-        }
-        if(col<colLen){
-            for(int i=rowLen-1;i>row;i--){
-                ls.add(matrix[i][col]);
+            for(int i=row+1;i<=rowLen;i++){
+                ls.add(matrix[i][colLen]);
             }
-        }
+            if(row<rowLen){
+                for(int i=colLen-1;i>=col;i--){
+                    ls.add(matrix[rowLen][i]);
+                }
+            }
+            if(col<colLen){
+                for(int i=rowLen-1;i>row;i--){
+                    ls.add(matrix[i][col]);
+                }
+            }
         row++;
         col++;
         rowLen--;
