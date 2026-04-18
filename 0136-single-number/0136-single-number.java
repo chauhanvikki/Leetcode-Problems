bc41c0323904d7821d161c412e11e185ad16fed5
Vishvendra Singh <112617024+chauhanvikki@users.noreply.github.com>
2026-04-18 11:30:18 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,23 +1,23 @@
-// class Solution {
-//     public int singleNumber(int[] nums) {
-//         int ans=0;
-//         for(int num: nums){
-//             ans ^= num;
-//         }
-//         return ans;
-//     }
-// }
-
 class Solution {
     public int singleNumber(int[] nums) {
         int ans=0;
-        HashMap<Integer,Integer> hm=new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            hm.put(nums[i],hm.getOrDefault(nums[i],0)+1);
+        for(int num: nums){
+            ans ^= num;
         }
-        for(int i=0;i<nums.length;i++){
-            if(hm.get(nums[i])==1)return nums[i];
-        }
-        return -1;
+        return ans;
     }
 }
+
+// class Solution {
+//     public int singleNumber(int[] nums) {
+//         int ans=0;
+//         HashMap<Integer,Integer> hm=new HashMap<>();
+//         for(int i=0;i<nums.length;i++){
+//             hm.put(nums[i],hm.getOrDefault(nums[i],0)+1);
+//         }
+//         for(int i=0;i<nums.length;i++){
+//             if(hm.get(nums[i])==1)return nums[i];
+//         }
+//         return -1;
+//     }
+// }
